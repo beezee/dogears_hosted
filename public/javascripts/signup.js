bddcc4d8721286742c847446b4597354b66ec400
beezee <brian.zeligson@gmail.com>
2012-06-08 06:17:16 -0400

    refactor signup.js and clear fields after signup attempt

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,13 +1,13 @@
 (function($, exports) {
     $('document').ready(function() {
         $('#signup_form').submit(function(e) {
+            var $uname = $('#user_name'), $pw = $('#password');
             e.preventDefault();
-            var pw = $.sha256($("#password").val());
-            var uname = $('#user_name').val();
-            $.post('./users/new', {u: uname, p: pw}, function(response) {
+            $.post('./users/new', {u: $uname.val(), p: $pw.val()}, function(response) {
                 var result = $.parseJSON(response);
                 if (result.status && result.status === 'success') alert("Thanks, you're all signed up, now download a browser extension.");
-                else alert("Oops- there was a problem creating your account.");
+                else alert(result.status);
+                $uname.val(''); $pw.val('');
             });
         });
     });
