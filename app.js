e8617f8af1bf94d52cea3a304cc8950650c67c06
beezee <brian.zeligson@gmail.com>
2012-06-08 05:02:08 -0400

    check for process.env.PORT

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , port = process.env.PORT || 3000;
 
 var app = module.exports = express.createServer();
 
@@ -31,6 +32,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
